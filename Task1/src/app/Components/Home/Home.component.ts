24a80aa096c4230644c36c28dd8042ef4601e867
rohitpol0911@gmail.com <rohitpol0911@gmail.com>
2024-07-04 10:56:56 +0530

    Setup of Modules and component

---
+++ Task1/src/app/Components/Home/Home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'home-comp',
+    templateUrl:'./Home.component.html',
+    styleUrls:['./Home.component.css']
+    // styleUrl:'./Home.component.css'
+})
+export class HomeComponent{
+
+}
